9a27a5b07ed562946322e773a29f7e6349889659
SteliosKarlatiras <stelioskarlatiras@gmail.com>
2024-02-29 17:37:50 +0200

    changes

--- ex0.py
+++ ex0.py
@@ -1,5 +1,5 @@
 import requests  # εισαγωγή της βιβλιοθήκης
-
+from datetime import datetime
 
 def more(text):
     count = 0
@@ -12,8 +12,40 @@ def more(text):
                 break
 
 
-url = 'http://python.org/'  # προσδιορισμός του url
+def server_status(url):
+    try:
+        if not url.startswith("http://") and not url.startswith("https://"):
+            url = 'http://' + url
 
-with requests.get(url) as response:  # το αντικείμενο response
-    html = response.text
-    more(html)
+        r = requests.head(url)
+        print("Server Software: " + r.headers.get('Server', 'N/A'))
+
+        cookies = r.cookies
+        if cookies:
+            print("Cookies: ")
+            for cookie in cookies:
+                print('Name: ', cookie.name, end="\t\t\t")
+                #print('Cookie expire: ', cookie.expires)
+                if cookie.expires:
+                    seconds = cookie.expires/1000
+                    print('Expires: ', datetime.fromtimestamp(seconds).strftime('%Y-%m-%d   '))
+
+        else:
+            print("Cookies not found")
+
+    except requests.exceptions.RequestException as e:
+        print("Error: ", e)
+
+
+def main():
+    url = input("Enter a URL: ")
+    server_status(url)
+    # with requests.get(url) as response:  # το αντικείμενο response
+    #    html = response.text
+    #    more(html)
+
+
+if __name__ == "__main__":
+    main()
+
+main()
